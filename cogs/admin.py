c03623b92e59c3b9e4201769222d23a810c784d8
duckist <70286349+duckist@users.noreply.github.com>
2022-01-17 03:38:57 +0530

    Added an eval command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,10 +1,12 @@
-import discord
+import discord, traceback, textwrap, io, subprocess, os
+from contextlib import redirect_stdout
 from discord.ext import commands
 
 class Admin(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self._last_result = None
 
     @commands.command(aliases=['re'])
     @commands.is_owner()
@@ -16,6 +18,51 @@ class Admin(commands.Cog):
         except:
             await ctx.channel.send(f'`{extension}` does not exist')
 
+    @commands.command(name="eval")
+    @commands.is_owner()
+    async def _eval(self, ctx, *, body: str):
+
+        env = {
+            'client': self.client,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 def setup(client):
     client.add_cog(Admin(client))
