bafa0cec0abf43a1154051329eadaccf9ad6fe74
duckist <70286349+duckist@users.noreply.github.com>
2022-02-03 03:08:57 +0530

    Added all intents

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def getPrefix(client, message):
 
     return prefixes[str(message.guild.id)]
 
-client = commands.Bot(command_prefix=getPrefix, help_command=None, case_insensitive=True)
+client = commands.Bot(command_prefix=getPrefix, help_command=None, case_insensitive=True, intents=discord.Intents().all())
 client._uptime = time()
 
 for filename in os.listdir('cogs'):
