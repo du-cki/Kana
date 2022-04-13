5b93883b50c58518641a5a39f1786953b8b4c3ab
70286349+duckist@users.noreply.github.com <70286349+duckist@users.noreply.github.com>
2022-04-14 03:47:16 +0400

    squashed more bugs and added a janky work around for images

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -12,13 +12,13 @@ class Snipe(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message : discord.Message):
-        if not message.author.bot and not message.attachments:
+        if not message.author.bot:
             self.del_msg[message.channel.id] = [message, discord.utils.utcnow()]
 
 
     @commands.Cog.listener()
     async def on_message_edit(self, before : discord.Message, after : discord.Message):
-        if not before.author.bot and not before.attachments and before.content != after.content:
+        if not before.author.bot and before.content != after.content:
             self.edit_msg[before.channel.id] = [before, discord.utils.utcnow()]
 
 
@@ -33,11 +33,16 @@ class Snipe(commands.Cog):
         """
 
         msg = self.del_msg.get(ctx.channel.id, None)
+
         if not msg:
             return await ctx.send("There is nothing for me to snipe here")
 
         embed = discord.Embed(color=0x2F3136, description=msg[0].content, timestamp=msg[1])
-        embed.set_author(name=str(msg[0].author), icon_url=msg[0].author.avatar.url)
+        
+        if msg[0].attachments:
+            embed.set_image(url=msg[0].attachments[0].url)
+
+        embed.set_author(name=str(msg[0].author), icon_url=msg[0].author.display_avatar)
         await ctx.send(embed=embed)
 
 
