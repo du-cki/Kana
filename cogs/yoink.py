418dbd3463e92db130a298bbf697f5a0a8f9bf1d
70286349+duckist@users.noreply.github.com <70286349+duckist@users.noreply.github.com>
2022-05-16 23:42:38 +0400

    separated the events for simplicity's sake

--- cogs/yoink.py
+++ cogs/yoink.py
@@ -6,29 +6,35 @@ from datetime import datetime
 import typing
 
 class Yoink(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
-    async def on_user_update(self, before : discord.Member, after : discord.Member):
+    async def on_user_username_update(self, before: discord.User, after: discord.User):
         if before.bot:
             return
-        
+
+        await self.bot.pool.execute("""
+        INSERT INTO users VALUES ($1, $2, $3)
+        """, before.id, discord.utils.utcnow().timestamp(), before.name)
+
+
+    @commands.Cog.listener()
+    async def on_user_update(self, before: discord.User, after: discord.User):
         if before.name != after.name:
-            await self.bot.pool.execute("""
-            INSERT INTO users VALUES ($1, $2, $3)
-            """, before.id, discord.utils.utcnow().timestamp(), before.name)
+            self.bot.dispatch("user_username_update", before, after)
 
         if before.avatar != after.avatar:
-            ... # will complete later
+            self.bot.dispatch("user_avatar_update", before, after)
+
 
     def format_time(self, time: int) -> str:
         return datetime.fromtimestamp(time).strftime("%a %d %b %Y %H:%M")
     
+
     @commands.command()
     @commands.is_owner()
-    async def avy(self, ctx : commands.Context, target: typing.Union[discord.Member, discord.User] = None):
+    async def avy(self, ctx: commands.Context, target: typing.Union[discord.Member, discord.User] = None):
         """
         Get's the username history of a user, displays accordingly in unix time.
         
@@ -39,7 +45,9 @@ class Yoink(commands.Cog):
         target = target or ctx.author
 
         q = await self.bot.pool.fetch("""
-        SELECT name, unix_time FROM users WHERE id = $1 ORDER BY unix_time DESC;
+        SELECT name, unix_time FROM users 
+        WHERE id = $1 
+        ORDER BY unix_time DESC;
         """, target.id)
 
         if not q:
