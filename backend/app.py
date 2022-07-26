3b8366470f9dbfea0a4b5ecfe0bf859b2b9f66fc
du-cki <70286349+du-cki@users.noreply.github.com>
2022-07-27 01:46:30 +0400

    fixed dumb logic

--- backend/app.py
+++ backend/app.py
@@ -11,13 +11,13 @@ config = Config(".env")
 
 class App(Starlette):
     def __init__(self, *args: Any, **kwargs: Any):
-        kwargs["on_startup"] = [self.start]
-        kwargs["on_shutdown"] = [self.close]
+        kwargs.setdefault("on_startup", []).append(self.start)
+        kwargs.setdefault("on_shutdown", []).append(self.close)
         super().__init__(*args, **kwargs) # type: ignore
 
     async def start(self) -> None:
         self.pool = await asyncpg.create_pool(dsn=config("PSQL_URI"))
-    
+
     async def close(self) -> None:
         if self.pool is not None: # linter
             await self.pool.close()
