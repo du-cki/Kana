3523f2e1132049ea6a911af539ae0414bc21d915
du-cki <du-cki>
2023-12-07 19:57:52 +0400

    Reformatted file

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -21,24 +21,25 @@ if TYPE_CHECKING:
 
 POKETWO_ID = 716390085896962058
 HINT_RE = re.compile("The pokémon is (?P<hint>.*).")
-CATCH_RE = re.compile("Congratulations <@!?(?P<catcher>[0-9]+)>! You caught a level (?P<level>[0-9]{1,2}) (?P<name>[a-zA-Zé. ]+)!(?: Added to Pokédex. You received (?P<reward>[0-9]+) Pokécoins!)?")
+CATCH_RE = re.compile(
+    "Congratulations <@!?(?P<catcher>[0-9]+)>! You caught a level (?P<level>[0-9]{1,2}) (?P<name>[a-zA-Zé. ]+)!(?: Added to Pokédex. You received (?P<reward>[0-9]+) Pokécoins!)?"
+)
+
 
 class Hint:
     def __init__(self, hint: str):
-        self.hint = list(
-            hint
-        )
+        self.hint = list(hint)
 
     def add(self, guess: Self):
         hint = str(guess)
         assert len(self.hint) == len(hint), "hint must've changed, length changed."
 
         for idx, letter in enumerate(hint):
-            if self.hint[idx] == '_' and self.hint[idx] != letter:
+            if self.hint[idx] == "_" and self.hint[idx] != letter:
                 self.hint[idx] = letter
 
     def __str__(self):
-        return ''.join(self.hint)
+        return "".join(self.hint)
 
 
 class Pokemon(BaseCog):
@@ -56,7 +57,9 @@ class Pokemon(BaseCog):
         }
 
     async def build_pokemon_table(self):
-        async with self.bot.session.get("https://raw.githubusercontent.com/poketwo/data/master/csv/pokemon.csv") as req:
+        async with self.bot.session.get(
+            "https://raw.githubusercontent.com/poketwo/data/master/csv/pokemon.csv"
+        ) as req:
             raw_csv = await req.text()
             reader = csv.reader(io.StringIO(raw_csv))
 
@@ -69,17 +72,21 @@ class Pokemon(BaseCog):
                     row[14],
                     row[16],
                     row[17],
-                )   
-    
+                )
+
                 final[en] = {"ja": ja, "ja_r": ja_r, "ja_t": ja_t, "de": de, "fr": fr}
 
-            assert final['name.en'] == {'ja': 'name.ja', 'ja_r': 'name.ja_r', 'ja_t': 'name.ja_t', 'de': 'name.de', 'fr': 'name.fr'} 
+            assert final["name.en"] == {
+                "ja": "name.ja",
+                "ja_r": "name.ja_r",
+                "ja_t": "name.ja_t",
+                "de": "name.de",
+                "fr": "name.fr",
+            }
             self.pokemon_table = final
 
-
     async def cog_load(self) -> None:
-        await self.build_pokemon_table() 
-
+        await self.build_pokemon_table()
 
     def guess(self, guess: Hint) -> list[str]:
         hint = str(guess)
@@ -92,30 +99,26 @@ class Pokemon(BaseCog):
 
         for i, letter in enumerate(hint):
             if letter != "_":
-                guesses = [
-                    p for p in guesses if p[i].lower() == letter.lower()
-                ]
+                guesses = [p for p in guesses if p[i].lower() == letter.lower()]
 
         return guesses
-
 
     def extract_hint(self, input: str) -> Optional[Hint]:
         match = HINT_RE.fullmatch(input)
         if not match:
             return None
 
-        return Hint(
-            match.groups()[0].replace("\\", "")
-        )
+        return Hint(match.groups()[0].replace("\\", ""))
 
-    
     @commands.group(name="guess", aliases=["hint"], invoke_without_command=True)
     async def _guess(self, ctx: "Context"):
         """
         Gives an estimating prediction from a `PokéTwo` hint.
         """
         if not self.pokemon_table:
-            raise commands.BadArgument("Pokémon table not build yet, please give me a few seconds.")
+            raise commands.BadArgument(
+                "Pokémon table not build yet, please give me a few seconds."
+            )
 
         hint = None
         if ctx.message.reference:
@@ -130,7 +133,9 @@ class Pokemon(BaseCog):
             hint = self.active_spawns[ctx.channel.id]
 
         if not hint:
-            return await ctx.send(f"Reply to a **PokéTwo** hint whilst using this command, or run **<@{POKETWO_ID}> hint** and I'll start slowly compiling a guess from the hints.")
+            return await ctx.send(
+                f'Reply to a **PokéTwo** hint whilst using this command, or run "**<@{POKETWO_ID}> hint**" and I\'ll start slowly compiling a guess from the hints.'
+            )
 
         guesses = self.guess(hint)
         if not guesses:
@@ -138,20 +143,20 @@ class Pokemon(BaseCog):
 
         if len(guesses) > 1:
             await ctx.send(
-                f"I've narrowed it down to these `{len(guesses)}` guesses:\n" +
-                ', '.join(f"\U0001f1ec\U0001f1e7 **{guess}**" for guess in guesses)
+                f"I've narrowed it down to these `{len(guesses)}` guesses:\n"
+                + ", ".join(f"\U0001f1ec\U0001f1e7 **{guess}**" for guess in guesses)
             )
         else:
             formatted: dict[str, str] = {
-                self.flags[k]: v
-                for k, v in self.pokemon_table[guesses[0]].items()
+                self.flags[k]: v for k, v in self.pokemon_table[guesses[0]].items()
             }
 
             await ctx.send(
-                f'\U0001f1ec\U0001f1e7 **{guesses[0]}**, ' + # so the english name is always first.
-                ', '.join(f"{k} **{v}**" for k, v in formatted.items() if v)
+                f"\U0001f1ec\U0001f1e7 **{guesses[0]}**, "
+                + ", ".join(  # so the english name is always first.
+                    f"{k} **{v}**" for k, v in formatted.items() if v
+                )
             )
-
 
     @_guess.command(hidden=True)
     @commands.is_owner()
@@ -166,7 +171,6 @@ class Pokemon(BaseCog):
 
         await msg.edit(content=f"rebuilt cache (took: `{end - start:.2}s`)")
 
-
     @commands.Cog.listener("on_message")
     async def poketwo_spawns(self, message: discord.Message):
         if message.author.id != POKETWO_ID:
@@ -175,7 +179,6 @@ class Pokemon(BaseCog):
         await self.check_poketwo_catch(message)
         await self.check_poketwo_spawn(message)
         await self.check_poketwo_hints(message)
-
 
     async def check_poketwo_hints(self, message: discord.Message):
         hint = self.extract_hint(message.content)
@@ -188,7 +191,6 @@ class Pokemon(BaseCog):
 
         self.active_spawns[message.channel.id] = hint
 
-
     async def check_poketwo_spawn(self, message: discord.Message):
         if not message.embeds:
             return
@@ -197,7 +199,6 @@ class Pokemon(BaseCog):
             return
 
         self.active_spawns[message.channel.id] = None
-
 
     async def check_poketwo_catch(self, message: discord.Message):
         match = CATCH_RE.fullmatch(message.content)
@@ -209,4 +210,4 @@ class Pokemon(BaseCog):
 
 
 async def setup(bot: "Bot"):
-    await bot.add_cog(Pokemon(bot)) 
+    await bot.add_cog(Pokemon(bot))
